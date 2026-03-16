8294a627be8224d364de36432baa3ce6c6e5a11b
Angeline G. Burrell <aburrell@users.noreply.github.com>
2026-03-16 17:35:57 -0400

    DOC: add DOI to headers
    Added the DOI and location of license information to the file headers.

--- pyIntensityFeatures/tests/test_instruments_satellites.py
+++ pyIntensityFeatures/tests/test_instruments_satellites.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# DOI: 10.5281/zenodo.15102100
 # Full license can be found in License.md
 #
 # DISTRIBUTION STATEMENT A: Approved for public release. Distribution is
