28f20814b54d8ac6465b7c1d4b7c4a5bdb8613c1
Angeline G. Burrell <aburrell@users.noreply.github.com>
2026-03-16 17:36:30 -0400

    DOC: added file header
    Added a whole file header to the test init file.

--- pyIntensityFeatures/tests/__init__.py
+++ pyIntensityFeatures/tests/__init__.py
@@ -1,1 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# DOI: 10.5281/zenodo.15102100
+# Full license can be found in License.md
+# ----------------------------------------------------------------------------
 """Initialization file for the unit and integration tests."""
